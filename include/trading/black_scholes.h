0e91b19f8c4a0c0a7c29d42a5087c1a23abc4096
Cody Michael Jones <46515483+cm-jones@users.noreply.github.com>
2024-06-16 02:18:43 -0500

    Add documentation

--- include/trading/black_scholes.h
+++ include/trading/black_scholes.h
@@ -24,9 +24,31 @@
 
 #pragma once
 
-enum OptionType { CALL, PUT };
+#include <cmath>
 
+/**
+ * @brief Option types
+ */
+enum OptionType {
+    CALL, /**< Call option */
+    PUT   /**< Put option */
+};
+
+/**
+ * @brief Black-Scholes model for option pricing
+ */
 class BlackScholes {
 public:
+    /**
+     * @brief Calculate the price of an option using the Black-Scholes model
+     *
+     * @param S Current stock price
+     * @param K Strike price
+     * @param T Time to maturity (in years)
+     * @param r Risk-free interest rate
+     * @param sigma Volatility
+     * @param type Option type (CALL or PUT)
+     * @return double Option price
+     */
     double calculate_option_price(double S, double K, double T, double r, double sigma, OptionType type);
 };
