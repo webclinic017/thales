e1dccb33b94e96c94ef7732f52bfe76f3b4fff20
Cody Michael Jones <46515483+cm-jones@users.noreply.github.com>
2024-06-18 11:51:49 -0500

    Fix format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Format all .h and .cpp files in the project
+# Navigate to the project root directory
+cd "$(dirname "$0")/.."
+
+# Find all .h and .cpp files and run clang-format on them
 find . -name '*.h' -o -name '*.cpp' | xargs clang-format -i
